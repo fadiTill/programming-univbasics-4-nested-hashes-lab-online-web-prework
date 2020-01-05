54cdb0a5bd3a83ddeb76c6edebd26c95f7d25fee
fadimata tillman <fadi.tillman@gmail.com>
2020-01-05 21:14:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,7 @@ def changing_alan
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+     } :alan_kay[languages]= "GUI"
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      

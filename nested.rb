542b53b53956f37daed46ec76a33e8d880dd8c8e
fadimata tillman <fadi.tillman@gmail.com>
2020-01-05 21:03:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -42,7 +42,7 @@ def alan_kay_is_known_for
 end
 
 def dennis_ritchies_language
-	programmer_hash = p:dennis
+	programmer_hash = p:dennis.[languages]
  		{
         :grace_hopper => {
           :known_for => "COBOL",
